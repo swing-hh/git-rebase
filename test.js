85d2bc11e968a21835af634fbf0f7ec01e5a8b61
yaojintao <yaojintao@zuoyebang.com>
2020-11-02 20:20:38 +0800

    恢复之前的版本

--- test.js
+++ test.js
@@ -14,17 +14,3 @@ setName('yaojintao1');
 
 
 var a = 100;
-var b = a + 100;
-
-var c = 100;
-
-var d = 1;
-var e = 2;
-var f = 3;
-
-// 我是主项目添加的提交
-var m = 11;
-// 这是M分支
-var n = 22;
-var q = 33;
-var o = 44;
