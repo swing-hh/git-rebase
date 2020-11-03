b9f5f0d7c492f1743925369643c7652d3269bbd2
yaojintao <yaojintao@zuoyebang.com>
2020-11-03 15:19:30 +0800

    2.0.0版本上线

--- test.js
+++ test.js
@@ -13,18 +13,3 @@ var nameStr =  'my name ' + myName;
 setName('yaojintao1');
 
 
-var a = 100;
-var c = 2;
-var b = 1;
-var d = 3;
-var e = 4;
-
-
-
-var bb = 2;
-
-
-var mmm = 'mmm';
-
-var aaa = 'aaa';
-var ccc = 'ccc';
